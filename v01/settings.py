8c9db837f84334253d12ff4a5c6ad7cf5aae5dbc
iw2been <168265101+iw2been@users.noreply.github.com>
2024-07-11 16:54:55 +0200

    Update settings.py

--- v01/settings.py
+++ v01/settings.py
@@ -1,5 +1,5 @@
 #def main():
-ffpath = "C:/Program Files/ffmpeg/bin/" #inserire qui il percorso del file ffmpeg.exe
+ffpath = "C:/Program Files/ffmpeg/bin/" #inserire qui il percorso del file ffmpeg.exe | insert here the correct path to ffmpeg executable
 
 #if __name__ == "__main__":
 #	main()
